e7627e95fc357bcc178a0a1b186303353777ee5e
Luiz Moraes <lffm1994@gmail.com>
2021-06-01 13:54:40 -0300

    Modulo 1 - Criando uma nova classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,17 @@ fun main() {
     println("Bem vindo ao Bytebank")
 
     testaComportamentosConta()
+
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "099.934.600-87",
+        salario = 2500.0
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println(("bonificacao ${alex.bonificacao()}"))
 }
 
 
